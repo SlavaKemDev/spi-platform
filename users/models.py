8f66e2cdddb1df05a380a77104c65a64be23fd15
Вячеслав Чертан <slavakemdev@gmail.com>
2026-03-21 12:03:26 +0300

    Fix order of REQUIRED_FIELDS in custom user model

--- users/models.py
+++ users/models.py
@@ -38,7 +38,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['last_name', 'first_name', 'patronymic']
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'patronymic']
 
     def __str__(self):
         return self.email
