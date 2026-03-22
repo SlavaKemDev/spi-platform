0d15faccf34cc38302f0a8d6ac84d0b06481429d
Вячеслав Чертан <slavakemdev@gmail.com>
2026-03-22 14:49:03 +0300

    feat: update settings for dynamic DEBUG and ALLOWED_HOSTS configuration

--- core/settings.py
+++ core/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False").lower() in ("True", "1", "true")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").replace(", ", ",").split(",") if not DEBUG else ["*"]
 
 
 # Application definition
